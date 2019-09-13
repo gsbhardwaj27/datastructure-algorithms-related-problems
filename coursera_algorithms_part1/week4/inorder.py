246d5cec492ed7fbd1dafeaec9d5efc12f8b1404
igauravb <gaurav@acelearningco.com>
2019-09-13 12:15:16 +0530

    Inorder and web tracking

--- coursera_algorithms_part1/week4/inorder.py
+++ coursera_algorithms_part1/week4/inorder.py
@@ -1,5 +1,11 @@
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
 def inorder(root):
     if root:
         inorder(root.left)
-        print(root.value)
+        print(root.key)
         inorder(root.right)
